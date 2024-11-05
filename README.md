3a71101fd3856d0624745dd7ed0938af53bc5cb1
SigmaRichards <danielscott.hess@gmail.com>
2024-11-05 18:40:25 +0800

    Rename and move elm compile to main.js

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Most of the information is thanks to this repo: <https://github.com/isaacvando/e
 
 The process is simple, the page sources are compiled separately into a build directory, then that build directory is deployed to pages.
 
-In the `src` directory, a subdirectory labelled `root` is taken as the root for the pages website.
+In the `apps` directory, a subdirectory labelled `root` is taken as the root for the pages website.
 
 There are additional steps I wish to take but this works for now.
--- build.sh
+++ build.sh
@@ -1,19 +1,51 @@
 #!/bin/bash
 
 ROOT_DIR=$(pwd)
-SRC_DIR="src"
+SRC_DIR="apps"
 ROOT_APP="root"
 BUILD_DIR="build"
+
+PREBUILT_DIR="build"
+SKELETON_FILE="index.html"
+DEFAULTS="defaults"
+
+build-app () {
+	c_dir=$1
+	mkdir -p $ROOT_DIR/$BUILD_DIR/$c_dir
+	if [[ -d $PREBUILT_DIR ]]; then
+		# Prebuilt
+		cp -r $PREBUILT_DIR/* $ROOT_DIR/$BUILD_DIR/$c_dir/.
+		return 0
+	fi
+	if [[ -f $SKELETON_FILE ]]; then
+		# Use supplied skeleton
+		elm make src/Main.elm --output="main.js" --optimize
+		rm -r elm-stuff
+		mv main.js $ROOT_DIR/$BUILD_DIR/$c_dir/.
+		cp $SKELETON_FILE $ROOT_DIR/$BUILD_DIR/$c_dir/.
+		return 0
+	fi
+	if [[ -f $ROOT_DIR/$DEFAULTS/$SKELETON_FILE ]]; then
+		# Use default skeleton
+		elm make src/Main.elm --output="main.js" --optimize
+		rm -r elm-stuff
+		mv main.js $ROOT_DIR/$BUILD_DIR/$c_dir/.
+		cp $ROOT_DIR/$DEFAULTS/$SKELETON_FILE $ROOT_DIR/$BUILD_DIR/$c_dir/.
+		return 0
+	fi
+	# Use elm generated index
+	elm make src/Main.elm
+	rm -r elm-stuff
+	mv index.html $ROOT_DIR/$BUILD_DIR/$c_dir/.
+	return 0
+}
 
 mkdir -p $BUILD_DIR
 cd $SRC_DIR
 for c_dir in ./*; do
 	if [[ -d $c_dir ]]; then
-		mkdir -p $ROOT_DIR/$BUILD_DIR/$c_dir
 		cd $c_dir
-		elm make src/Main.elm
-		cp index.html $ROOT_DIR/$BUILD_DIR/$c_dir/.
-		rm -r elm-stuff
+		build-app $c_dir
 		cd ..
 	fi
 done
